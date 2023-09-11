0a606cf57a6a023ac01047e0cdc2f95c83d8207c
nakamaboy <144308244+nakamaboy@users.noreply.github.com>
2023-09-11 13:34:42 +0800

    files uploaded

---
+++ Lust for Truth/Assets/Scripts/newmove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newmove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController controller;
+    public Transform cam;
+    public float speed = 6f;
+
+    public float turnsmoothtime = 0.1f;
+
+    float turnSmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        
+    }
+}
